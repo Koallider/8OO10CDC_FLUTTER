dac264af2c36bbc4a1dddbc670d078c2d7fc1ba6
koallider <xehxtkf@gmail.com>
2022-11-06 23:19:27 +0300

    Letter decoration and focus

--- lib/base_widgets/letter.dart
+++ lib/base_widgets/letter.dart
@@ -1,6 +1,7 @@
 import 'package:countdown_solver/theme.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class LetterWidget extends StatefulWidget {
@@ -8,7 +9,8 @@ class LetterWidget extends StatefulWidget {
   final bool editable;
   final Function? onChanged;
 
-  LetterWidget({Key? key, required this.letter, this.editable = false, this.onChanged})
+  LetterWidget(
+      {Key? key, required this.letter, this.editable = false, this.onChanged})
       : super(key: key);
 
   @override
@@ -16,7 +18,6 @@ class LetterWidget extends StatefulWidget {
 }
 
 class _LetterWidgetState extends State<LetterWidget> {
-
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -26,17 +27,31 @@ class _LetterWidgetState extends State<LetterWidget> {
       alignment: Alignment.center,
       child: !widget.editable
           ? Text(
-              widget.letter.length > 1 ? widget.letter.substring(0, 1) : widget.letter,
+              widget.letter.length > 1
+                  ? widget.letter.substring(0, 1)
+                  : widget.letter,
               textAlign: TextAlign.center,
               style: getStyle(),
             )
           : TextField(
-              onChanged: (value){
-                //todo getter in widget?
+              onChanged: (value) {
                 widget.letter = value;
                 widget.onChanged?.call(value);
+                if(value.isNotEmpty){
+                  FocusScope.of(context).nextFocus();
+                }else{
+                  FocusScope.of(context).previousFocus();
+                }
               },
+              decoration: const InputDecoration(
+                hintText: "",
+                counterText: "",
+              ),
+              inputFormatters: [
+                LengthLimitingTextInputFormatter(1),
+              ],
               maxLength: 1,
+              maxLengthEnforcement: MaxLengthEnforcement.enforced,
               style: getStyle(),
               textAlign: TextAlign.center,
             ),
