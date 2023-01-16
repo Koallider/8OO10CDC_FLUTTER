0cdecbc6436a42eb42ca5747cb6c0bc8228abfa0
koallider <xehxtkf@gmail.com>
2023-01-16 10:37:52 +0300

    number solver tests

--- test/number_solver_test.dart
+++ test/number_solver_test.dart
@@ -2,5 +2,29 @@ import 'package:countdown_solver/solver/number_solver/number_solver.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
-
+  group("Number solver tests", () {
+    test("No solution test", (){
+      var solver = NumberSolver(target: 333, nums: [1, 2]);
+      var result = solver.solve();
+      expect(result.length, equals(0));
+    });
+    test("2 numbers test", (){
+      var solver = NumberSolver(target: 6, nums: [2, 3]);
+      var result = solver.solve();
+      expect(result.length, equals(1));
+      expect(result, containsAll(["3*2"]));
+    });
+    test("3 numbers test", (){
+      var solver = NumberSolver(target: 16, nums: [2, 4, 6]);
+      var result = solver.solve();
+      expect(result.length, equals(2));
+      expect(result, containsAll(["(6*2)+4", "(6-2)*4"]));
+    });
+    test("Duplicate number test", (){
+      var solver = NumberSolver(target: 4, nums: [2, 2]);
+      var result = solver.solve();
+      expect(result.length, equals(2));
+      expect(result, containsAll(["2*2", "2+2"]));
+    });
+  });
 }
